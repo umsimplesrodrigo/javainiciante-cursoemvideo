c965b8d2d27f7a5b411111f4f2b63ad136308d37
umsimplesrodrigo <umsimplesti@gmail.com>
2025-02-23 03:50:15 -0300

    Inserindo continação das aulas

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author suporte-01
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /*int n1 = 3;
+       int n2 = 5;
+       float m = (n1 + n2) / 2;
+       System.out.printf("A media entre %d e %d = %.2f\n", n1, n2, m);*/
+       
+       /*int numero = 5;
+       int valor = 4 + numero++;
+       
+       System.out.println(valor);*/
+       
+       /*int x = 4;
+       x *= 2; // x = x * 2
+       
+       System.out.println(x);*/
+       
+       /*float v = (float) 8.9;
+       int ar = (int) Math.ceil(v);
+       System.out.println(ar);*/
+       
+       /*double ale = Math.random();
+       int n = (int) Math.round(5 + ale * (10 - 5));
+       System.out.println(n);*/
+       
+       float n1 = 5;
+       float n2 = 2;
+       double resultado = n1/n2;
+       System.out.println(resultado);
+    }
+    
+}
